53b247bf5176d23d58aa6b8220450b764ef49231
Hibeekay1261 <oyewoleibukun12@gmail.com>
2022-05-24 21:52:54 +0100

    first commit

---
+++ build/precache-manifest.04a9ba7af731c96ac27f163d09ccdf05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "171f2ebb491e3695c8c424d8b0071524",
+    "url": "/index.html"
+  },
+  {
+    "revision": "756a1f7fd4160c895cd5",
+    "url": "/static/css/main.3c2fef4c.chunk.css"
+  },
+  {
+    "revision": "f090046ec967bd71fa12",
+    "url": "/static/js/2.60eb6cee.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.60eb6cee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "756a1f7fd4160c895cd5",
+    "url": "/static/js/main.7f661971.chunk.js"
+  },
+  {
+    "revision": "2a5e9718c304aba39f84",
+    "url": "/static/js/runtime-main.0f5541c3.js"
+  },
+  {
+    "revision": "b0a1eb7cef456f10aa4ae48bbf9827d8",
+    "url": "/static/media/logo.b0a1eb7c.png"
+  }
+]);
